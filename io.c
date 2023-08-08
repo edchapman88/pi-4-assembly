989ab4d94f48cf0e812b390c1bbf9dc19547fd59
Ed Chapman - Turing <edchapmanelc@gmail.com>
2023-08-08 20:40:53 +0100

    uart init in assembly

--- io.c
+++ io.c
@@ -62,23 +62,24 @@ enum {
     AUX_MU_CNTL_REG = AUX_BASE + 96,
     AUX_MU_BAUD_REG = AUX_BASE + 104,
     AUX_UART_CLOCK  = 500000000,
-    UART_MAX_QUEUE  = 16 * 1024
+    UART_MAX_QUEUE  = 16 * 1024,
+    TEST = ((AUX_UART_CLOCK/(115200*8))-1)
 };
 
 #define AUX_MU_BAUD(baud) ((AUX_UART_CLOCK/(baud*8))-1)
 
 void uart_init() {
-    mmio_write(AUX_ENABLES, 1); //enable UART1
-    mmio_write(AUX_MU_IER_REG, 0);
-    mmio_write(AUX_MU_CNTL_REG, 0);
-    mmio_write(AUX_MU_LCR_REG, 3); //8 bits
-    mmio_write(AUX_MU_MCR_REG, 0);
-    mmio_write(AUX_MU_IER_REG, 0);
-    mmio_write(AUX_MU_IIR_REG, 0xC6); //disable interrupts
-    mmio_write(AUX_MU_BAUD_REG, AUX_MU_BAUD(115200));
-    gpio_useAsAlt5(14);
-    gpio_useAsAlt5(15);
-    mmio_write(AUX_MU_CNTL_REG, 3); //enable RX/TX
+    // mmio_write(AUX_ENABLES, 1); //enable UART1
+    // mmio_write(AUX_MU_IER_REG, 0);
+    // mmio_write(AUX_MU_CNTL_REG, 0);
+    // mmio_write(AUX_MU_LCR_REG, 3); //8 bits
+    // mmio_write(AUX_MU_MCR_REG, 0);
+    // mmio_write(AUX_MU_IER_REG, 0);
+    // mmio_write(AUX_MU_IIR_REG, 0xC6); //disable interrupts
+    // mmio_write(AUX_MU_BAUD_REG, AUX_MU_BAUD(115200));
+    // gpio_useAsAlt5(14);
+    // gpio_useAsAlt5(15);
+    // mmio_write(AUX_MU_CNTL_REG, 3); //enable RX/TX
 }
 
 unsigned int uart_isWriteByteReady() { return mmio_read(AUX_MU_LSR_REG) & 0x20; }
